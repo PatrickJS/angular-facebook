984ac233f0be9d3f943c25661c84758b57597402
gdi2290 <github@gdi2290.com>
2013-11-01 19:30:39 -0700

    allow user to configure asyc loading of Facebook SDK

--- angular-facebook.js
+++ angular-facebook.js
@@ -4,5 +4,26 @@
 /* angular Facebook */
 module
 
+module.provider('FB', function() {
+  var _asyncLoading = false;
+  var _scriptUrl = '//connect.facebook.net/en_US/all.js';
+  var _scriptId = 'facebook-jssdk';
+
+  this.asyncLoading = function(config) {
+    _asyncLoading = config || _asyncLoading;
+    return this;
+  };
+
+  this.scriptUrl = function(url, id) {
+    _scriptUrl = url || _scriptUrl;
+    _scriptId = id || _scriptId;
+    return this;
+  };
+
+  this.scriptId = function(id) {
+    _scriptId = id || _scriptId;
+    return this;
+  };
+});
 
 }(angular.module('angular-facebook', []), angular, window);
