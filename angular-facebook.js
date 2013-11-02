fe64667ba841d0c4dea01a180467a954ac6f2bb2
gdi2290 <github@gdi2290.com>
2013-11-01 19:31:01 -0700

    create helper function for creating the script tag

--- angular-facebook.js
+++ angular-facebook.js
@@ -24,6 +24,25 @@ module.provider('FB', function() {
     _scriptId = id || _scriptId;
     return this;
   };
+
+  // Create a script tag with moment as the source
+  // and call our onScriptLoad callback when it
+  // has been loaded
+  function createScript($document, callback) {
+    var scriptTag = $document.createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.async = true;
+    scriptTag.id = _scriptId;
+    scriptTag.src = _scriptUrl;
+    scriptTag.onreadystatechange = function () {
+      if (this.readyState == 'complete') {
+        callback();
+      }
+    }
+    scriptTag.onload = callback;
+    var s = $document.getElementsByTagName('body')[0];
+        s.appendChild(scriptTag);
+  }
 });
 
 }(angular.module('angular-facebook', []), angular, window);
